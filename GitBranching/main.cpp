a265524c38ebf4197671d7903330d290bdd0d708
Kata Almasy <almasy.kata@gmail.com>
2017-01-13 14:12:57 +0100

    a1 and giginore refreshed

--- GitBranching/main.cpp
+++ GitBranching/main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void greet(string name) {
+  cout << "Hello " << name << "!" <<endl;
+}
+
 int main() {
+  greet("Kata");
   return 0;
 }
