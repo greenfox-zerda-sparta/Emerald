f95b2d5f87e36230d76dc513e5a6c2495f4a77fb
katbtr <bisztrayk@gmail.com>
2017-01-30 09:43:44 +0100

    QT Server with Logging

---
+++ Qt Server/MyTime.cpp
@@ -0,0 +1,27 @@
+﻿#include <ctime>
+#include <string>
+
+#include "MyTime.h"
+#include <time.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+MyTime::MyTime() {}
+
+//std::string Time::GetCurrentTime() {
+//  time_t     now = time(0);
+//  struct tm  tstruct;
+//  char       buf[80];
+//  tstruct = *localtime(&now);
+//  strftime(buf, sizeof(buf), "%Y-%m-%d %X ", &tstruct);
+//  return buf;
+//}
+
+std::string MyTime::GetTimeFileFormat() {
+  time_t     now = time(0);
+  struct tm  tstruct;
+  char       buf[80];
+  tstruct = *localtime(&now);
+  strftime(buf, sizeof(buf), "%Y-%m-%d %H-%M-%S", &tstruct);
+  return buf;
+}
