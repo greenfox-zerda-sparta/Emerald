ac0de85e3a6127ca8d8d2a3c2e15f45362ed8c3d
katbtr <bisztrayk@gmail.com>
2017-02-06 11:57:53 +0100

    Device class added

---
+++ Qt Server/Device.cpp
@@ -0,0 +1,78 @@
+#include "Device.h"
+
+
+Device::Device()
+{
+  
+}
+
+Device::~Device()
+{
+  
+}
+
+Device::Device(unsigned char deviceIDHigh, unsigned char devideIDLow)
+{
+  
+}
+
+void Device::set_deviceIDHigh()
+{
+  deviceIDHigh = deviceCommandMap.at("DeviceIDHigh");
+}
+
+void Device::set_deviceIDLow()
+{
+  deviceIDLow = deviceCommandMap.at("DeviceIDHigh");
+}
+
+unsigned char Device::get_deviceIDHigh()
+{
+  return deviceIDHigh;
+}
+
+unsigned char Device::get_deviceIDLow()
+{
+  return deviceIDLow;
+}
+
+void Device::connect_Device()
+{
+  
+}
+
+void Device::disconnect_Device()
+{
+  
+}
+
+void Device::run_Main_Function(unsigned char mainFunctionCommand)
+{
+  std::string deviceMainStatus;
+
+  if (mainFunctionCommand == 0)
+  {
+    deviceMainStatus = "OFF";
+  } else
+  {
+    deviceMainStatus = "ON";
+  }
+
+  send_Device_Main_Status_Message(deviceMainStatus);
+
+}
+
+std::string Device::send_Device_Main_Status_Message(std::string device_Main_Status)
+{
+  return device_Main_Status;
+}
+
+void Device::run_Second_Function(unsigned char secondFunctionCommand)
+{
+
+}
+
+void Device::run_Third_Function(unsigned char thirdFunctionCommand)
+{
+
+}
---
+++ Qt Server/Device.h
@@ -0,0 +1,35 @@
+#ifndef DEVICE_H
+#define DEVICE_H
+
+#include "server.h"
+
+class Device
+{
+private:
+  unsigned char deviceIDHigh;
+  unsigned char deviceIDLow;
+  std::unordered_map<std::string, unsigned char> deviceCommandMap;
+  unsigned char mainFunctionCommand;
+  unsigned char secondFunctionCommand;
+  unsigned char thirdFunctionCommand;
+  bool isSecondFunction;
+  bool isThirdFunction;
+public:
+  Device();
+  Device(unsigned char, unsigned char);
+  ~Device();
+  void set_deviceIDHigh();
+  void set_deviceIDLow();
+  unsigned char get_deviceIDHigh();
+  unsigned char get_deviceIDLow();
+  void connect_Device();
+  void disconnect_Device();
+  virtual void run_Main_Function(unsigned char) = 0;
+  virtual void run_Second_Function(unsigned char);
+  virtual void run_Third_Function(unsigned char);
+  std::string send_Device_Main_Status_Message(std::string device_Main_Status);
+
+};
+
+
+#endif
