820bd4e3c46c23dd45824861d20d9c6920d6ce1b
katbtr <bisztrayk@gmail.com>
2017-02-08 16:43:57 +0100

    Group ID removed from device constructor, defined only in subdevice (Lamp) class constructor.

--- Qt Server/QT Server/Lamp.cpp
+++ Qt Server/QT Server/Lamp.cpp
@@ -1,17 +1,8 @@
 #include "Lamp.h"
 
-Lamp::Lamp()
-{
 
-}
-
-Lamp::~Lamp()
-{
-
-}
-
-Lamp::Lamp(byte deviceIDHigh, byte devideIDLow)
-{
-
+Lamp::Lamp(byte _deviceIDHigh, byte _deviceIDLow, byte _homeID, byte _floorID, byte _roomID, byte _cmdID, std::string _IP) 
+: Device(_deviceIDHigh, _deviceIDLow, _homeID, _floorID, _roomID, _cmdID, _IP) {
+  groupID = '1';
 }
 
--- Qt Server/QT Server/Lamp.h
+++ Qt Server/QT Server/Lamp.h
@@ -9,18 +9,9 @@ typedef unsigned char byte;
 class Lamp : public Device
 {
 private:
-  std::unordered_map<std::string, byte> deviceCommandMap;
-  byte mainFunctionCommand;
-  byte secondFunctionCommand;
-  byte thirdFunctionCommand;
-  bool isSecondFunction;
-  bool isThirdFunction;
+
 public:
-  Lamp();
-  Lamp(byte, byte);
-  ~Lamp();
-  void set_groupID();
-  byte get_groupID();
+  Lamp(byte _deviceIDHigh, byte _deviceIDLow, byte _homeID, byte _floorID, byte _roomID, byte _cmdID, std::string _IP);
 
 };
 
