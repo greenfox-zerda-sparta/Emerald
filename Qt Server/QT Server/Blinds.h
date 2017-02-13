f50dd0070e22069eb5398970e478009f23f25268
katbtr <bisztrayk@gmail.com>
2017-02-13 11:04:04 +0100

    Refactor: Devices ID struct added

--- Qt Server/QT Server/Blinds.h
+++ Qt Server/QT Server/Blinds.h
@@ -7,7 +7,7 @@ class Blinds : public Device {
 private:
 
 public:
-  Blinds(byte _deviceIDHigh, byte _deviceIDLow, byte _homeID, byte _floorID, byte _roomID, byte _cmdID, std::string _IP);
+  Blinds(IDs _ids, std::string _IP);
 };
 
 
--- Qt Server/QT Server/GarageDoor.h
+++ Qt Server/QT Server/GarageDoor.h
@@ -7,7 +7,7 @@ class GarageDoor : public Device {
 private:
 
 public:
-  GarageDoor(byte _deviceIDHigh, byte _deviceIDLow, byte _homeID, byte _floorID, byte _roomID, byte _cmdID, std::string _IP);
+  GarageDoor(IDs _ids, std::string _IP);
 };
 
 
