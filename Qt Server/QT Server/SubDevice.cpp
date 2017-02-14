0798faefae0b45c17f9b28f4f1f7117b9d9696b7
katbtr <bisztrayk@gmail.com>
2017-02-14 13:06:34 +0100

    Device and UI logging added, seems to be working, logging into a separate file

--- Qt Server/QT Server/SubDevice.cpp
+++ Qt Server/QT Server/SubDevice.cpp
@@ -1,10 +1,19 @@
 #include "SubDevice.h"
+#include "messageconverter.h"
 
 SubDevice::SubDevice(IDs _ids, std::string _IP)
   : Device(_ids, _IP) {
+  bool isdeviceworking = true;
   mydevicelogfile = new Logfile;
 
-//  mydevicelogfile->device_log_buffer(LocalTimer->GetTimeFileFormat() + _ids + _IP);
+  mydevicelogfile->device_log_buffer(
+    toString(int(get_homeID())) + " " +
+    toString(int(get_floorID())) + " " +
+    toString(int(get_roomID())) + " " +
+    toString(int(get_groupID())) + " " +
+    toString(int(get_deviceIDHigh())) + " " +
+    toString(int(get_deviceIDLow())) + " " +
+    _IP + toString(isdeviceworking) + "\n");
 }
 
 SubDevice::~SubDevice() {
--- Qt Server/QT Server/UI.cpp
+++ Qt Server/QT Server/UI.cpp
@@ -1,4 +1,5 @@
 #include "UI.h"
+#include "Logfile.h"
 
 UI::UI(IDs _ids, std::string _IP)
 : Device(_ids, _IP){
@@ -8,5 +9,20 @@ UI::UI(IDs _ids, std::string _IP)
   homeID = 255;
   floorID = 255;
   roomID = 255;
+
+  bool isdeviceworking = true;
+  mydevicelogfile = new Logfile;
+
+  mydevicelogfile->device_log_buffer(
+    toString(int(get_homeID())) + " " +
+    toString(int(get_floorID())) + " " +
+    toString(int(get_roomID())) + " " +
+    toString(int(get_groupID())) + " " +
+    toString(int(get_deviceIDHigh())) + " " +
+    toString(int(get_deviceIDLow())) + " " +
+    _IP + toString(isdeviceworking) + "\n");
 }
 
+UI::~UI() {
+  delete mydevicelogfile;
+}
